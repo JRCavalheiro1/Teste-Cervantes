5ca7d72f92e3be55c5e2f12879087fdfe8856c8e
Julio Ricardo <juliomilcharek519@gmail.com>
2025-02-06 15:44:02 -0300

    added form validation

--- TesteCervantes/Form1.Designer.cs
+++ TesteCervantes/Form1.Designer.cs
@@ -29,12 +29,99 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(238, 63);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Nome";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(238, 129);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(38, 13);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Senha";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(241, 79);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(100, 20);
+            this.nameInput.TabIndex = 2;
+            this.nameInput.Validating += new System.ComponentModel.CancelEventHandler(this.nameInput_Validating);
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Location = new System.Drawing.Point(241, 145);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.Size = new System.Drawing.Size(100, 20);
+            this.passwordInput.TabIndex = 3;
+            this.passwordInput.Validating += new System.ComponentModel.CancelEventHandler(this.passwordInput_Validating);
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button.Location = new System.Drawing.Point(230, 197);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(119, 34);
+            this.button.TabIndex = 4;
+            this.button.Text = "Cadastre-se";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(577, 348);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "Form1";
+            this.Text = "Cadastro";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
     }
 }
 
--- TesteCervantes/Form1.cs
+++ TesteCervantes/Form1.cs
@@ -16,5 +16,63 @@ namespace TesteCervantes
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            String userName = nameInput.Text;
+            int userPassword = Convert.ToInt32(passwordInput.Text);
+        }
+
+        private void nameInput_Validating(object sender, CancelEventArgs e)
+        {
+            if(String.IsNullOrEmpty(nameInput.Text))
+            {
+                setError(nameInput, errorProvider1, "Este campo é obrigatório!", e);
+            }
+            else
+            {
+                clearError(nameInput, errorProvider1);
+            }
+        }
+
+        private void passwordInput_Validating(object sender, CancelEventArgs e)
+        {
+            if (String.IsNullOrEmpty(passwordInput.Text))
+            {
+                setError(passwordInput, errorProvider2, "Este campo é obrigatório!", e);
+                return;
+            }
+
+
+            if (!int.TryParse(passwordInput.Text, out _))
+            {
+                setError(passwordInput, errorProvider2, "A senha deve somente conter números", e);
+                return;
+            }
+
+            if (passwordInput.TextLength < 2)
+            {
+                setError(passwordInput, errorProvider2, "A senha deve ter pelo menos 2 caracteres!", e);
+                return;
+            }
+
+            clearError(passwordInput, errorProvider2);
+        }
+
+        private void setError(System.Windows.Forms.TextBox input, ErrorProvider provider, String message, CancelEventArgs e)
+        {
+            provider.SetError(input, message);
+            input.BackColor = Color.IndianRed;
+            input.ForeColor = Color.White;
+            e.Cancel = true;
+        }
+
+        private void clearError(System.Windows.Forms.TextBox input, ErrorProvider provider)
+        {
+            provider.SetError(input, "");
+            input.BackColor = Color.White;
+            input.ForeColor = Color.Black;
+        }
+
     }
 }
